c403bce6f53ada775f3c55659d44ff940de8aec6
Prachi Ikhar <prachiikhar42@gmail.com>
2025-01-03 11:40:35 +0530

    Hey I added A file

--- Practice.js
+++ Practice.js
@@ -6,3 +6,17 @@ document.write('\n', cars);
 document.write("<br>");
 
 document.write(cars.slice());
+
+let arr = ["mangoo", "apple"];
+for (i of arr) {
+    console.log(i);
+}
+let p = [
+    ["a", "e", "i", "o", "u"],
+    ["pldkjo", "jhjjh"]
+];
+for (i of p) {
+    for (p of i) {
+        console.log("This is andar ka" + p);
+    }
+}
