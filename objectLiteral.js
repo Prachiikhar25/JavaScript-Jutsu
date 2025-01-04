ad0d0e9afb7a9e4170dfe975da2761de898a9614
Prachi Ikhar <prachiikhar42@gmail.com>
2025-01-04 16:55:08 +0530

    Added changes

---
+++ objectLiteral.js
@@ -0,0 +1,12 @@
+const Student = {
+    name: "Prachi",
+    sem: 5,
+    age: 19
+};
+//object literals can also store array
+//const matlab hm student ye naam change nahi kar sakte hain.
+let item = {
+    price: 11,
+    items: ["paani", "puri"]
+};
+console.log(item["price"]);
